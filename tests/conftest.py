4eab7123baa384c9d09c6a0d87efc9f05672ca1c
for-you-Nikita <troickiy09@gmail.com>
2025-10-09 12:02:36 +0300

    добавление headless режима правильное

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,8 @@ def set_up():
     print('Начало теста')
     options = webdriver.ChromeOptions()
     options.add_experimental_option('detach', True)
+    options.add_argument("--headless") # Открытие браузера в headless режиме, то есть без открытия браузера
+    options.add_argument('--headless=old') # Открытие браузера в headless режиме, то есть без открытия браузера
 
     driver = webdriver.Chrome(options=options)
     driver.get('https://litte-dev-shop.appprod.ru/collection/all')
